c57a08510b4db0a2362bc19456e6bedb406172d4
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-02-18 23:35:48 +0900

    feat: argument switch

--- bot/alias/alias.go
+++ bot/alias/alias.go
@@ -1,7 +1,6 @@
 package alias
 
 import (
-	"fmt"
 	"github.com/approvers/rusty-ponyo/bot"
 )
 
@@ -23,7 +22,38 @@ func (b *messageAliasBot) OnMessage(msg bot.Message, ctx bot.Context) {
 	case noMatch:
 	case parseError:
 		ctx.SendMessage(parsed.errorMsg)
+	case dataAvailable:
+		ctx.SendMessage(onCommand(parsed.data))
+	}
+}
+
+func help() string {
+	const helpText string = "```asciidoc\n" +
+		"= rusty_ponyo::alias =\n" +
+		"g!alias [subcommand] [args...]\n" +
+		">>> 引数において \" は省略できません <<<\n" +
+		"= subcommands =\n" +
+		"    help                         :: この文を出します\n" +
+		"    make \"[キー]\" \"[メッセージ]\" :: エイリアスを作成します\n" +
+		"    delete \"[キー]\"              :: エイリアスを削除します\n" +
+		"```"
+
+	return helpText
+}
+
+func onCommand(data parseData) string {
+	if data.subCommand == "" {
+		return help()
 	}
 
-	fmt.Printf("%#v\n", parsed)
+	switch data.subCommand {
+	case "make":
+		return "called make"
+	case "delete":
+		return "called delete"
+	case "help":
+		fallthrough
+	default:
+		return help()
+	}
 }
