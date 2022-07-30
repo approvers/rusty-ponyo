ab66a6918b5d365c7c56fad01822af9104179fb0
kawaemon <me@kawaemon.dev>
2022-07-31 01:31:11 +0900

    feat(gh): make proper UI

--- src/bot/gh/mod.rs
+++ src/bot/gh/mod.rs
@@ -1,33 +1,134 @@
 use {
     crate::bot::{BotService, Context, Message},
     anyhow::{Context as _, Result},
-    async_hofs::prelude::*,
     async_trait::async_trait,
+    clap::{Args, CommandFactory, Parser},
+    derivative::Derivative,
     once_cell::sync::Lazy,
     regex::Regex,
+    reqwest::StatusCode,
     std::{fmt::Write, path::Path},
     url::Url,
 };
 
+const NAME: &str = "rusty_ponyo::bot::gh";
+const PREFIX: &str = "g!gh";
+
+/// GitHub のコードリンクからプレビューを生成します
+#[derive(Debug, clap::Args)]
+#[clap(name=NAME, about, long_about=None)]
+struct Ui {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+impl Ui {
+    fn command<'a>() -> clap::Command<'a> {
+        clap::Command::new(NAME).bin_name(PREFIX)
+    }
+}
+impl Parser for Ui {}
+impl CommandFactory for Ui {
+    fn into_app<'help>() -> clap::Command<'help> {
+        Self::augment_args(Self::command())
+    }
+    fn into_app_for_update<'help>() -> clap::Command<'help> {
+        Self::augment_args_for_update(Self::command())
+    }
+}
+
+#[derive(Debug, clap::Subcommand)]
+enum Command {
+    /// ヘルプメッセージを出します
+    Help,
+
+    /// 指定されたリンクのプレビューを生成します。
+    Preview {
+        /// コードのリンク
+        /// 例: https://github.com/approvers/rusty-ponyo/blob/02bb011de7d06e242a275dd9a9126a21effc6854/Cargo.toml#L48-L52
+        url: String,
+    },
+}
+
+#[derive(Derivative)]
+#[derivative(Debug)]
+enum PreviewError {
+    NoUrlDetected,
+    FetchError { status_code: StatusCode },
+    InternalError(#[derivative(Debug = "ignore")] anyhow::Error),
+}
+
 pub struct GitHubCodePreviewBot;
 
-#[async_trait]
-impl BotService for GitHubCodePreviewBot {
-    fn name(&self) -> &'static str {
-        "rusty_ponyo::bot::gh"
+impl GitHubCodePreviewBot {
+    async fn on_command(&self, message: &str, ctx: &dyn Context) -> Result<()> {
+        use Command::*;
+
+        let words = match shellwords::split(message) {
+            Ok(w) => w,
+            Err(_) => {
+                return ctx
+                    .send_text_message("閉じられていない引用符があります")
+                    .await
+                    .context("failed to send message")
+            }
+        };
+
+        let parsed = match Ui::try_parse_from(words) {
+            Ok(p) => p,
+            Err(e) => {
+                return ctx
+                    .send_text_message(&format!("```{e}```"))
+                    .await
+                    .context("failed to send message")
+            }
+        };
+
+        match parsed.command {
+            // handled by clap
+            Help => Ok(()),
+
+            Preview { url } => {
+                let preview_result = self.gen_preview(&url).await;
+
+                match preview_result {
+                    Ok(ref p) => ctx.send_text_message(&p).await,
+                    Err(ref e) => {
+                        ctx.send_text_message(&format!("couldn't generate preview: ```{e:#?}```"))
+                            .await
+                    }
+                }
+                .context("failed to send message")?;
+
+                if let Err(PreviewError::InternalError(e)) = preview_result {
+                    return Err(e.context("failed to generate preview: internal error"));
+                }
+
+                Ok(())
+            }
+        }
     }
 
-    async fn on_message(&self, msg: &dyn Message, ctx: &dyn Context) -> anyhow::Result<()> {
-        let links = CodePermalink::find_from_str(msg.content());
+    async fn gen_preview(&self, message: &str) -> Result<String, PreviewError> {
+        let links = CodePermalink::find_from_str(message);
         if links.is_empty() {
-            return Ok(());
+            return Err(PreviewError::NoUrlDetected);
         }
 
         let mut msg = String::new();
 
-        macro_rules! w { ($($arg:tt)*) => { let _ = writeln!(msg, $($arg)*); } }
-
         for link in links {
+            let code = link.get_code().await.map_err(|e| {
+                if let Some(r) = e.downcast_ref::<reqwest::Error>() &&
+                   let Some(status_code) = r.status() {
+                        PreviewError::FetchError{status_code}
+                    } else {
+                        PreviewError::InternalError(e)
+                    }
+            })?;
+
+            macro_rules! w { ($($arg:tt)*) => { let _ = writeln!(msg, $($arg)*); } }
+
             w!(
                 "{}/{} [{}] : {}",
                 link.user,
@@ -36,13 +137,30 @@ impl BotService for GitHubCodePreviewBot {
                 link.path
             );
             w!("```{}", link.ext);
-            w!("{}", link.get_code().await?);
-            w!("```\n");
+            w!("{}", code);
+            w!("```");
         }
 
-        ctx.send_text_message(&msg.trim())
-            .await
-            .context("failed to send message")?;
+        Ok(msg)
+    }
+}
+
+#[async_trait]
+impl BotService for GitHubCodePreviewBot {
+    fn name(&self) -> &'static str {
+        NAME
+    }
+
+    async fn on_message(&self, msg: &dyn Message, ctx: &dyn Context) -> anyhow::Result<()> {
+        if msg.content().starts_with(PREFIX) {
+            return self.on_command(msg.content(), ctx).await;
+        }
+
+        if let Ok(msg) = self.gen_preview(msg.content()).await {
+            ctx.send_text_message(&msg)
+                .await
+                .context("failed to send message")?;
+        }
 
         Ok(())
     }
@@ -69,7 +187,7 @@ impl CodePermalink {
         URL_REGEX
             .find_iter(msg)
             .flat_map(|m| {
-                // e.g.: https://github.com/serenity-rs/serenity/blob/current/src/framework/mod.rs#L105
+                // e.g.: https://github.com/approvers/rusty-ponyo/blob/02bb011de7d06e242a275dd9a9126a21effc6854/Cargo.toml#L48-L52
                 let url = Url::parse(m.as_str()).ok()?;
 
                 macro_rules! bail {
@@ -121,7 +239,10 @@ impl CodePermalink {
 
         let code = reqwest::get(rawcode_url)
             .await
-            .async_and_then(|r| async move { r.text().await })
+            .context("failed to make request")?
+            .error_for_status()
+            .context("response had bad status code")?
+            .text()
             .await
             .context("failed to download rawcode")?;
 
@@ -194,7 +315,6 @@ optional = true
 default-features = false
 features = ["rustls_backend", "client", "gateway", "model", "cache"]
 ```
-
 approvers/rusty-ponyo [02bb011de7d06e242a275dd9a9126a21effc6854] : Cargo.toml
 ```toml
 version = "0.10"
@@ -209,7 +329,8 @@ features = ["rustls-tls"]
 
 [dependencies.tokio]
 version = "1"
-```"#
+```
+"#
             );
 
             self.called.store(true, Ordering::Relaxed);
