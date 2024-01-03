6502b10f29a7945b2cbbdc35c4a4641431baff9b
Nanai Jua <me@n7i.dev>
2024-01-04 08:09:22 +0900

    feat: re-add `GenkaiPointFormulaV3`

--- src/bot/genkai_point/formula/v3.rs
+++ src/bot/genkai_point/formula/v3.rs
@@ -11,54 +11,48 @@ impl GenkaiPointFormula for FormulaV3 {
         let jdt = session.joined_at.with_timezone(&Tokyo);
         let ldt = session.left_at().with_timezone(&Tokyo);
 
-        const ONE_DAY_MILLIS: i64 = 24 * 60 * 60 * 1000;
-        let c = jdt.timestamp_millis() % ONE_DAY_MILLIS;
-        let c = c as f64 / ONE_DAY_MILLIS as f64;
+        const ONE_HOUR_MILLIS: i64 = 60 * 60 * 1000;
 
-        let pts = (0..(ldt - jdt).num_minutes())
-            .map(|t| t as f64 / 60.0)
-            .map(|t| formula(c, t))
-            .sum::<f64>();
+        let c = (jdt.timestamp_millis() % (24 * ONE_HOUR_MILLIS)) as f64 / ONE_HOUR_MILLIS as f64;
+        let t = (ldt - jdt).num_milliseconds() as f64 / ONE_HOUR_MILLIS as f64;
 
-        pts as u64
+        ((formula(c, t) - formula(c, 0.0)) * 10.0).round() as u64
     }
 }
 
-// weight-duration graph (rendered on web): https://www.geogebra.org/graphing/vcczhr5s
+#[rustfmt::skip]
 fn formula(c: f64, t: f64) -> f64 {
-    const PI: f64 = core::f64::consts::PI;
+    let pi = core::f64::consts::PI;
 
-    (4.0 / PI)
-        * (t / 5.0 + 1.0).log2()
-        * ((-t / 5.0).atan() + PI / 2.0)
-        * ((((t + 5.0 + c) / 12.0 * PI).sin() + 1.0) / 2.0).powi(2)
-}
+    let sin = f64::sin;
+    let cos = f64::cos;
+    let exp = f64::exp;
+    let pow = f64::powi;
 
-// これはテストではないのでテストではない
-#[test]
-fn session_test() {
-    use crate::bot::genkai_point::datetime;
+    let pi2 = pow(pi, 2);
 
-    macro_rules! session_test {
-        ($tag:literal : $jdt:expr => $ldt:expr) => {{
-            let session = Session {
-                user_id: 0,
-                joined_at: $jdt,
-                left_at: Some($ldt),
-            };
+    let pi2_36 = pi2 + 36.0;
+    let pi2_09 = pi2 +  9.0;
 
-            println!(
-                "{} : <{} [min]> {} <x{}>",
-                $tag,
-                ($ldt - $jdt).num_minutes(),
-                FormulaV3.calc(&session),
-                FormulaV3.calc(&session) as f64 / ($ldt - $jdt).num_minutes() as f64
-            );
-        }};
-    }
+    let pi2_36_2 = pow(pi2_36, 2);
+    let pi2_09_2 = pow(pi2_09, 2);
 
-    session_test!("very sh" : datetime!(2021/1/2 00:00:00) => datetime!(2021/1/2 01:00:00));
-    session_test!("short  " : datetime!(2021/1/1 21:00:00) => datetime!(2021/1/2 23:00:00));
-    session_test!("trend  " : datetime!(2021/1/1 20:00:00) => datetime!(2021/1/2 03:00:00));
-    session_test!("daytime" : datetime!(2021/1/1 10:00:00) => datetime!(2021/1/2 20:00:00));
+    let tc5_pi_06 = ((t + c + 5.0) * pi /  6.0).rem_euclid(pi * 2.0);
+    let tc5_pi_12 = ((t + c + 5.0) * pi / 12.0).rem_euclid(pi * 2.0);
+
+    let ex0 =        pi * pi2_36_2 * (pi2_09 * t + 36.0             ) * sin(tc5_pi_06);
+    let ex1 = -3.0 *      pi2_36_2 * (pi2_09 * t + 18.0  - 2.0 * pi2) * cos(tc5_pi_06);
+    let ex2 = 48.0 *      pi2_09_2 * (pi2_36 * t + 72.0  - 2.0 * pi2) * sin(tc5_pi_12);
+    let ex3 =  8.0 * pi * pi2_09_2 * (pi2_36 * t + 144.0            ) * cos(tc5_pi_12);
+
+    let ex4 = pi2_36_2 * pi2_09_2 * t;
+
+    let ex5 =      2.0 * pow(pi, 8);
+    let ex6 =    180.0 * pow(pi, 6);
+    let ex7 =   5346.0 * pow(pi, 4);
+    let ex8 =  58320.0 * pow(pi, 2);
+    let ex9 = 209952.0             ;
+
+    let ex = ex4 + ex9 + ex8 + ex5 + ex3 + ex2 + ex1 + ex0 + ex7 + ex6;
+    -1.0 * (3.0 * exp((-t + 2.0) / 2.0) * ex) / (8.0 * pi2_36_2 * pi2_09_2)
 }
