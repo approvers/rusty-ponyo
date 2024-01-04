ff94f5622a9dcecc26796020a6aa6e394f912327
Nanai Jua <me@n7i.dev>
2024-01-05 06:02:32 +0900

    feat: replace calculation from `f64` to `BigDecimal`

--- src/bot/genkai_point/formula/v3.rs
+++ src/bot/genkai_point/formula/v3.rs
@@ -1,6 +1,7 @@
 use {
     super::{GenkaiPointFormula, GenkaiPointFormulaOutput},
     crate::bot::genkai_point::model::Session,
+    bigdecimal::{BigDecimal as Real, FromPrimitive as _, ToPrimitive as _},
     chrono_tz::Asia::Tokyo,
 };
 
@@ -23,24 +24,27 @@ impl GenkaiPointFormula for FormulaV3 {
             .map(|(jdt, ldt)| {
                 const ONE_HOUR_MILLIS: i64 = 60 * 60 * 1000;
 
-                let c = (jdt.timestamp_millis() % (24 * ONE_HOUR_MILLIS)) as f64
-                    / ONE_HOUR_MILLIS as f64;
+                let c = Real::from(jdt.timestamp_millis() % (24 * ONE_HOUR_MILLIS))
+                    / Real::from(ONE_HOUR_MILLIS);
 
-                let t = (ldt - jdt).num_milliseconds() as f64 / ONE_HOUR_MILLIS as f64;
+                let t = Real::from((ldt - jdt).num_milliseconds()) / Real::from(ONE_HOUR_MILLIS);
+
+                let n23 = Real::from(23);
+                let n00 = Real::from(0);
 
                 #[rustfmt::skip]
-                let now_point = formula(   c, t) - formula(   c, 0.0);
-                let max_point = formula(23.0, t) - formula(23.0, 0.0);
+                let now_point = formula(  c.clone(), t.clone()) - formula(  c.clone(), n00.clone());
+                let max_point = formula(n23.clone(), t.clone()) - formula(n23.clone(), n00.clone());
 
                 (now_point, max_point)
             })
             .unzip::<_, _, Vec<_>, Vec<_>>();
 
-        let now_point = now_points.iter().sum::<f64>() * 10.0;
-        let max_point = max_points.iter().sum::<f64>() * 10.0;
+        let now_point: Real = now_points.iter().sum::<Real>() * 10;
+        let max_point: Real = max_points.iter().sum::<Real>() * 10;
 
-        let point = now_point.round() as u64;
-        let efficiency = now_point / max_point;
+        let point = now_point.round(0).to_u64().unwrap();
+        let efficiency = (now_point / max_point).to_f64().unwrap();
 
         GenkaiPointFormulaOutput { point, efficiency }
     }
@@ -49,38 +53,39 @@ impl GenkaiPointFormula for FormulaV3 {
 #[rustfmt::skip]
 // rendered: https://www.geogebra.org/graphing/esdsm7rz
 // used for integrate: https://www.integral-calculator.com
-fn formula(c: f64, t: f64) -> f64 {
-    let pi = core::f64::consts::PI;
+fn formula(c: Real, t: Real) -> Real {
+    let pi = Real::from_f64(core::f64::consts::PI).unwrap();
 
-    let sin = f64::sin;
-    let cos = f64::cos;
-    let exp = f64::exp;
-    let pow = f64::powi;
+    let sin = |n: Real| Real::from_f64(n.to_f64().unwrap().sin()).unwrap();
+    let cos = |n: Real| Real::from_f64(n.to_f64().unwrap().cos()).unwrap();
 
-    let pi2 = pow(pi, 2);
+    let exp  = |n: Real| n.exp();
+    let pow2 = |n: Real| n.square();
 
-    let pi2_36 = pi2 + 36.0;
-    let pi2_09 = pi2 +  9.0;
+    let pi2 = pow2(pi.clone());
 
-    let pi2_36_2 = pow(pi2_36, 2);
-    let pi2_09_2 = pow(pi2_09, 2);
+    let pi2_36: Real = pi2.clone() + 36;
+    let pi2_09: Real = pi2.clone() +  9;
 
-    let tc5_pi_06 = ((t + c + 5.0) * pi /  6.0).rem_euclid(pi * 2.0);
-    let tc5_pi_12 = ((t + c + 5.0) * pi / 12.0).rem_euclid(pi * 2.0);
+    let pi2_36_2 = pow2(pi2_36.clone());
+    let pi2_09_2 = pow2(pi2_09.clone());
 
-    let ex0 =        pi * pi2_36_2 * (pi2_09 * t + 36.0             ) * sin(tc5_pi_06);
-    let ex1 = -3.0 *      pi2_36_2 * (pi2_09 * t + 18.0  - 2.0 * pi2) * cos(tc5_pi_06);
-    let ex2 = 48.0 *      pi2_09_2 * (pi2_36 * t + 72.0  - 2.0 * pi2) * sin(tc5_pi_12);
-    let ex3 =  8.0 * pi * pi2_09_2 * (pi2_36 * t + 144.0            ) * cos(tc5_pi_12);
+    let tc5_pi_06: Real = ((t.clone() + c.clone() + 5) * pi.clone() /  6) % pi.double();
+    let tc5_pi_12: Real = ((t.clone() + c.clone() + 5) * pi.clone() / 12) % pi.double();
 
-    let ex4 = pi2_36_2 * pi2_09_2 * t;
+    let ex0 =      pi.clone() * pi2_36_2.clone() * (pi2_09.clone() * t.clone() + 36                   ) * sin(tc5_pi_06.clone());
+    let ex1 = -3 *              pi2_36_2.clone() * (pi2_09.clone() * t.clone() + 18  - 2 * pi2.clone()) * cos(tc5_pi_06.clone());
+    let ex2 = 48 *              pi2_09_2.clone() * (pi2_36.clone() * t.clone() + 72  - 2 * pi2.clone()) * sin(tc5_pi_12.clone());
+    let ex3 =  8 * pi.clone() * pi2_09_2.clone() * (pi2_36.clone() * t.clone() + 144                  ) * cos(tc5_pi_12.clone());
 
-    let ex5 =      2.0 * pow(pi, 8);
-    let ex6 =    180.0 * pow(pi, 6);
-    let ex7 =   5346.0 * pow(pi, 4);
-    let ex8 =  58320.0 * pow(pi, 2);
-    let ex9 = 209952.0             ;
+    let ex4 = pi2_36_2.clone() * pi2_09_2.clone() * t.clone();
+
+    let ex5 =      2 * (pi2.clone() * pi2.clone() * pi2.clone() * pi2.clone());
+    let ex6 =    180 * (pi2.clone() * pi2.clone() * pi2.clone()              );
+    let ex7 =   5346 * (pi2.clone() * pi2.clone()                            );
+    let ex8 =  58320 * (pi2.clone()                                          );
+    let ex9 = 209952;
 
     let ex = ex4 + ex9 + ex8 + ex5 + ex3 + ex2 + ex1 + ex0 + ex7 + ex6;
-    -1.0 * (3.0 * exp((-t + 2.0) / 2.0) * ex) / (8.0 * pi2_36_2 * pi2_09_2)
+    -1 * (3 * exp((-t + 2) / 2) * ex) / (8 * pi2_36_2 * pi2_09_2)
 }
